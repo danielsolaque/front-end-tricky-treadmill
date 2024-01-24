b8d37ab1e39b5dff088ae08b06ab0b4335582188
Daniel Solaque <danielsolaque@pursuit.org>
2024-01-24 08:07:02 -0500

    initializing core components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import NavBar from "./Components/NavBar/NavBar"
+import Home from "./Components/Home/Home"
 
 function App() {
   return (
-    <BrowserRouter >
+    <BrowserRouter>
+      <NavBar />
       <Routes>
-        <Route path="/" element={ 
-          <div>
-            Hola mundo
-          </div>
-        } /> 
-        <Route path="/machine" element={<div>
-          Machine Route
-        </div>}/>
+        <Route path="/" element={<Home/>} />
+        <Route
+          path="/brands"
+          element={
+            <div>This is the list of brands where the reviews come from.</div>
+          }
+        />
+        <Route
+          path="/models"
+          element={<div>This is teh list of models per brand.</div>}
+        />
+        <Route
+          path="/reviews"
+          element={<div>This is the list of reviews per model</div>}
+        />
+        <Route
+          path="/review_creation"
+          element={<div>New Form for review.</div>}
+        />
+        <Route path="/review_edit" element={
+          <div>Here Im going to edit my reviews</div>
+        } />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,26 @@
+
+import "./Home.scss"
+
+export default function Home() {
+    return (
+        <div>
+            <div className="home">
+                NavBar
+            </div>
+            <div>
+                Showing all brands -Carroussel-
+            </div>
+            <div>
+                Showing top most use full reviews
+            </div>
+            <div>
+                Leaving a review.
+            </div>
+            <div>
+                FootBar
+            </div>
+        </div>
+    )
+    
+
+}
